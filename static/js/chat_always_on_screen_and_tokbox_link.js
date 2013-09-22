07d94b40472ca2475335b4b7c43b3384d32e9727
John McLear <john@mclear.co.uk>
2013-09-22 21:11:53 +0100

    fix URL and require AV = yes

--- static/js/chat_always_on_screen_and_tokbox_link.js
+++ static/js/chat_always_on_screen_and_tokbox_link.js
@@ -16,15 +16,16 @@ var chat_always_on_screen_and_tokbox_link = {
     var tokbox_link = clientVars.tokbox_link;
     if(tokbox_link){
       if (chat_always_on_screen_and_tokbox_link.getURLParameter("av")){  // is it set?
-
-        var link = tokbox_link + "?string="+location.href;
-        var markup = '<p id="tokboxStart" style="cursor:pointer; cursor:hand;text-align:center;text-size:10px;font-weight:bold;text-transform:uppercase;padding-bottom:8px;">START VIDEO/AUDIO CHAT</p>';
-        $(markup).insertBefore("#chatinput");
-        $("#tokboxStart").click(function(){
-          window.open(link, 'utilization', 'height=700,width=350,scrollbars=1');
-        });
-
-        $("#chattext").css("bottom", "120px");
+        if(chat_always_on_screen_and_tokbox_link.getURLParameter("av") === "YES"){
+          var link = tokbox_link + "?string="+location.href;
+          var markup = '<p id="tokboxStart" style="cursor:pointer; cursor:hand;text-align:center;text-size:10px;font-weight:bold;text-transform:uppercase;padding-bottom:8px;">START VIDEO/AUDIO CHAT</p>';
+          $(markup).insertBefore("#chatinput");
+          $("#tokboxStart").click(function(){
+            window.open(link, 'utilization', 'height=700,width=350,scrollbars=1');
+          });
+  
+          $("#chattext").css("bottom", "120px");
+        }
       }
     }
   }
